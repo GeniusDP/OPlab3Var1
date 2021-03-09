90d72e4ed164b03e2f3be54fa918192c857f8dda
GeniusDP <zaranikbz@gmail.com>
2021-03-09 20:11:03 +0200

    Закомментировал main

--- main.cpp
+++ main.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
 //#include "fileIO.h"
-//#include "dejkstra.h"
-//#include "preority_queue.h"
+#include "dejkstra.h"
 
 using namespace std;
 
 /*
-    //очередь пар pair<int, int>
+    очередь пар pair<int, int>
 
     PreorityQueue q;//сейчас пустой
+        сортируется по возрастанию приоритетов(от маленьких к большим)
 
-        q.insert(<приоритет>, <доп информация>);
+
+        (70, 2)
+        (90, 1)
+
+        q.push(<приоритет>, <доп информация>);
 
         q.empty();
 
         q.pop(); - удалить первый элемент
 
-        q.extract(<приоритет>, <доп информация>); - удалить конкретную пару где-то в очереди
-
-        q.head() - получать указатель на первый элемент
+        q.top() - получать указатель на первый элемент
 
 */
 
 int main()
 {
-    int rows, cols;
-    int map[100][100]={};
-    int stRow, stCol;
-    int finRow, finCol;
-    readFromFile(map, rows, cols, stRow, stCol, finRow, finCol);
-
-    findShortestWay(map, rows, cols, stRow, stCol, finRow, finCol);
-
-    writeToFile(map, rows, cols);
+//    int rows, cols;
+//    int map[100][100]={};
+//    int stRow=5, stCol=1;
+//    int finRow=1, finCol=6;
+//    readFromFile(map, rows, cols);
+//
+//    findShortestWay(map, rows, cols, stRow, stCol, finRow, finCol);
+//
+//    writeToFile(map, rows, cols);
     return 0;
 }
