923ee89eb843fe0ae16fb3d5fad5e30689dbd2e2
GeniusDP <zaranikbz@gmail.com>
2021-03-09 21:07:52 +0200

    Вывод NO SOLUTION или проложение маршрута.

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void writeToWindow(int map[][100], int n, int m){
--- main.cpp
+++ main.cpp
@@ -33,7 +33,10 @@ int main()
 
     readFromFile(map, rows, cols);
     vector<int> parent = dejkstra(map, rows, cols, stRow*rows+stCol, finRow*rows+finCol);
-    findWayFromStartToFinish(parent, finRow*rows+finCol, map, rows, cols);
-    writeToWindow(map, rows, cols);
+    if(parent[finRow*rows+finCol]==-1)cout << "NO SOLUTION!";
+    else{
+        findWayFromStartToFinish(parent, finRow*rows+finCol, map, rows, cols);
+        writeToWindow(map, rows, cols);
+    }
     return 0;
 }
