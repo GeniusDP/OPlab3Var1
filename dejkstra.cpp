ce8e3593a8078d397c017577d807b1ef3dea2055
GeniusDP <zaranikbz@gmail.com>
2021-03-09 21:07:15 +0200

    Убрали минус перед d[to]. В нашей реализации очереди с приоритетом это не требуется.

--- dejkstra.cpp
+++ dejkstra.cpp
@@ -10,20 +10,17 @@ void convertSumFormToIndexes(int& x, int& y, int sumForm, int n){
 }
 
 void oneRelaxation(int map[][100], int x, int y, int v, int n, vector<int>& d, vector<int>& p, priority_queue < pair<int,int> >& q){
-    if( map[x][y] != -1 ){
+        if( map[x][y] != -1 ){
             int to = x*n + y;
             if (d[v] + 1 < d[to]) {
-        d[to] = d[v] + 1;
-        p[to] = v;
-        q.push (make_pair (-d[to], to));
-      }
+                d[to] = d[v] + 1;
+                p[to] = v;
+                q.push (make_pair (d[to], to));
+            }
         }
 }
 
 vector<int> dejkstra(int map[][100], int n, int m, int st, int fin){
-//    int st = 5*n+1;
-//    int fin = 1*n+6;
-
     vector<int> d(n*m, INF), p(n*m, -1);
     d[st] = 0;
     priority_queue < pair<int,int> > q;
@@ -45,14 +42,6 @@ vector<int> dejkstra(int map[][100], int n, int m, int st, int fin){
         oneRelaxation(map, x, y-1, v, n, d, p, q);
         oneRelaxation(map, x, y+1, v, n, d, p, q);
     }
-    //    vector< pair<int, int> > way(1, make_pair(fin/n, fin%n));
-    //    int i=fin;
-    //    while(p[i] != -1){
-    //        way.push_back(make_pair(p[i]/n, p[i]%n));
-    //        i=p[i];
-    //    }
-    //    reverse(way.begin(), way.end());
-    //    return way;
     return p;
 }
 
